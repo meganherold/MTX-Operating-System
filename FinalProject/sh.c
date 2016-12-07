ac5d8dbc7a2e421504e84ba012cda69d63274130
Megan <meganmcp95@gmail.com>
2016-12-06 18:24:25 -0800

    pipe and redirect appear fine for now, need further testing.

--- FinalProject/sh.c
+++ FinalProject/sh.c
@@ -27,8 +27,9 @@ int is_built_in(char* command, char* arg1)
 
 int do_pipe(char *commandline, int *pipe_descriptor)
 {
-  int has_pipe = 0;
-  char *head, *tail;
+  int has_pipe = 0, pid;
+  int local_pipe[2];
+  char *head = "", *tail = "";
 
   if(pipe_descriptor) //if a pipe was passed in, we're WRITER on pipe pd
   {
@@ -42,9 +43,126 @@ int do_pipe(char *commandline, int *pipe_descriptor)
 
   if(has_pipe)
   {
-    
+    //create a local pipe
+    pipe(local_pipe);
+    pid = fork();
+
+    if(pid) //parent as reader
+    {
+      close(local_pipe[1]);
+      dup2(local_pipe[0], 0);
+      close(local_pipe[0]);
+      do_command(tail);
+    }
+
+    else //we have more piping to do! recursive call.
+      do_pipe(head, local_pipe);
   }
 
+  else //no pipe here, just a regular executable thing
+    do_command(commandline);
+
+}
+
+int do_command(char *commandline)
+{
+  int num_tokens = 0, i, redirect_location, in, out;
+  char line_copy[300], new_command[300], *line_tokens[10], *command;
+  char* redirect = "";
+
+  printf("megan's do_command\n");
+
+  //first, break commandline up into tokens
+  strcpy(line_copy, commandline);
+
+  line_tokens[num_tokens] = strtok(line_copy, " ");
+  while(line_tokens[num_tokens] != 0 && num_tokens < 10)
+  {
+    //printf("sh: token = %s\n", line_tokens[num_tokens]);
+    num_tokens++;
+    line_tokens[num_tokens] = strtok(0, " "); //line_tokens will end in null
+  }
+
+  command = line_tokens[0];
+
+  //look for IO redirection symbols
+  for(i = 0; i < num_tokens; i++)
+  {
+    //we know we'll only ever have one per do_command call, because of do_pipe
+    if(strcmp(line_tokens[i], "<") == 0   //take input from a thing
+    || strcmp(line_tokens[i], ">") == 0   // write output to a thing
+    || strcmp(line_tokens[i], ">>") == 0) //append output to a thing
+    {
+      redirect_location = i;
+      redirect = line_tokens[i];
+      printf("redirect found: %s at %d\n", redirect, redirect_location);
+      break;
+    }
+  }
+
+  //do IO redirections
+  if(strcmp(redirect, "<") == 0) //take input from a thing
+  {
+    //printf("attempting to open %s for reading.\n", line_tokens[redirect_location + 1]);
+    close(0);   //close stdin
+    in = open(line_tokens[redirect_location + 1], O_RDONLY); //open the thing as 0
+
+    if(in < 0)
+    {
+      printf("failed to open file %s for reading.\n", line_tokens[redirect_location + 1]);
+      exit(0);
+    }
+
+    printf("opened %s for reading.\n", line_tokens[redirect_location + 1]);
+  }
+
+  else if(strcmp(redirect, ">") == 0) //write output to a thing
+  {
+    //printf("attempting to open %s for writing.\n", line_tokens[redirect_location + 1]);
+    close(1);   //close stdout
+
+    //open for writing OR create the thing if it doesn't exist
+    out = open(line_tokens[redirect_location + 1], O_WRONLY | O_CREAT);
+
+    if(out < 0)
+    {
+      printf("failed to open/create file %s for writing.\n", line_tokens[redirect_location + 1]);
+      exit(0);
+    }
+    printf("opened %s for writing.\n", line_tokens[redirect_location + 1]);
+  }
+
+  else if(strcmp(redirect, ">>") == 0) //append output to a thing
+  {
+    //printf("attempting to open %s for writing.\n", line_tokens[redirect_location + 1]);
+    close(1);   //close stdout
+
+    //open for writing OR appending OR create the thing if it doesn't exist
+    out = open(line_tokens[redirect_location + 1], O_WRONLY | O_APPEND | O_CREAT);
+
+    if(out < 0)
+    {
+      printf("failed to open/create file %s for appending.\n", line_tokens[redirect_location + 1]);
+      exit(0);
+    }
+    printf("opened %s for appending.\n", line_tokens[redirect_location + 1]);
+  }
+
+  //now build a command line without redirection symbols
+  //(because we just took care of those) and send it to exec
+  strcpy(new_command, line_tokens[0]);
+  for(i = 1; i < num_tokens; i++)
+  {
+    if(i != redirect_location)
+    {
+      strcat(new_command, " ");
+      strcat(new_command, line_tokens[i]);
+    }
+  }
+
+  printf("do_command: excecuting %s\n", new_command);
+  exec(new_command);
+  return 1;
 }
 
 int scan(char *commandline, char **head, char **tail)
@@ -52,6 +170,7 @@ int scan(char *commandline, char **head, char **tail)
   //divide command line into head, tail by rightmost pipe symbol
   char *pointer = commandline;
 
+  //printf("scanning...\n");
   //set pointer to the end of the command line
   while(*pointer)
     pointer++;
@@ -62,7 +181,10 @@ int scan(char *commandline, char **head, char **tail)
 
   //did we find any | ?
   if(pointer == commandline)
+  {
+    //printf("scan: no | were found.\n");
     return 0; //if we've reached the beginning, no | were found.
+  }
 
   //we found one! there's a pipe here
   *pointer = 0;          //turn the | into NULL so that we can set commandline to head
@@ -115,6 +237,6 @@ int main(int argc, char *argv[])
       else
         do_pipe(line, 0);
     }
-
+    printf("bottom of while loop\n");
   }
 }
